c63a3417cd64ae5e8c666ee2a3f1ad6acb5d0c92
Jerry <calljerryli@outlook.com>
2018-05-15 21:22:34 +0800

    init

---
+++ FuckZhihuishuMeetingClass.js
@@ -0,0 +1,49 @@
+function myAjaxjsonp(url, param, jsoncallback, callback, errorCallback) {
+    $.ajax({
+        url: url,
+        type: "get",
+        data: param,
+        dataType: "jsonp",
+        jsonp: "callback",
+        jsonpCallback: jsoncallback,
+        success: function (data) {
+            callback(data);
+        },
+        error: function (e) {
+            if (errorCallback) {
+                errorCallback(e);
+            }
+        }
+    });
+}
+
+var mySubmitSignTimes = function (relativeTime, end) {
+    var param = {
+        "courseId": liveCourseId,
+        "userId": userId,
+        "relativeTime": relativeTime,
+        "watchType": "2",
+        "jsonpCallBack": "jsonpCallBack",
+        "videoId": curVideoId,
+        "sourceType": 1
+    };
+
+    myAjaxjsonp(
+        appUrl + "/live/setWatchHistory",
+        param,
+        param.jsonpCallBack,
+        function (data) {
+            console.log(relativeTime + ":success:" + data);
+            relativeTime++;
+            if (relativeTime > end) {
+                return;
+            }
+            mySubmitSignTimes(relativeTime, end);
+        },
+        function (e) {
+            console.log(relativeTime + ":error:" + e);
+        }
+    );
+};
+
+mySubmitSignTimes(1, 100)
